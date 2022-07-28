24690f7411872eda46512691b30fffa184c5526c
Antonio Busuladzich <antonio.busuladzich@gmail.com>
2022-07-28 08:57:09 +0300

    Day4.2.1

--- Day4/main.cpp
+++ Day4/main.cpp
@@ -16,5 +16,19 @@ int main() {
 	cout << char(~'q' << 6) << endl;
 	unsigned long ul1 = 3, ul2 = 7;
 	cout << (ul1 | ul2) << endl;
+	//sizeof
+	//Note size of returns the value of the type in bytes;
+	cout << "sizeof" << endl;
+	long i = 5, *iPtr = &i;
+	cout << (sizeof i) << endl << (sizeof iPtr) << endl << (sizeof * iPtr) << endl;;
+	int arr[3] = { 16,15,21 };
+	cout << (sizeof arr) << endl << (sizeof * arr) << endl << (sizeof "string") << endl << (sizeof 's');
+	/*
+	
+	*/
+
+	int x[10]; int* p = x;
+	cout << sizeof(x) << " " << (sizeof(x) / sizeof(*x)) << endl;
+	cout << sizeof(p) << " " << (sizeof(p) / sizeof(*p)) << endl;
 	return 0;
 }
